c9e082277ef8c51ab71c79defce7d834b5a93bc6
minxuezh <55081293+minxuezh@users.noreply.github.com>
2019-11-30 16:06:00 +0800

    Add files via upload

--- linebox/app.py
+++ linebox/app.py
@@ -33,12 +33,12 @@ file.close()
 from flask import * #載入 flask 模組
 app=Flask("My Website") # 建立一個網站應用程式物件
 #網站的網址:http://主機名稱/路徑?參數名稱=資料&參數名稱=資料&...
-#例如:http://127.0.0.1:5000/
+#例如: https://lineboxshu.herokuapp.com/
 @app.route("/")# 指定對應的網址路徑
 def home(): # 對應的處理函式
-    return "<h3>Hello Flask</h3><div>This is line 1</div><script>alert('Hello');</script>"  # 回應給前端的訊息
+    return render_template("home.html") # 回應給前端的訊息
 
-#例如:http://127.0.0.1:5000/test.php?keyword=關鍵字
+#例如: https://lineboxshu.herokuapp.com/test.php?keyword=關鍵字
 @app.route("/test.php")# 指定對應的網址路徑
 def test(): # 對應的處理函式
     # 取得網址列上的參數:request.args.get(參數名稱, 預設值)
@@ -47,8 +47,9 @@ def test(): # 對應的處理函式
         return redirect("/") #導向到路徑 /
     else:
         if keyword in data:
-            return "中文 "+data[keyword]
+            return render_template("result.html", CHINESE=data[keyword])
         else:
-            return "沒有翻譯"
+            return render_template("result.html", CHINESE="沒有翻譯")
 
-app.run() # 啟動伺服器
+if __name__=="__main__": # 如果以主程式執行，立即啟動伺服器
+    app.run() # 啟動伺服器
